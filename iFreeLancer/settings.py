52a8f6b24bcb4e02b47e8c694b3d35207ea9659d
Musafir-19 <rkhunkarov@mail.ru>
2022-01-25 11:55:22 +0300

    emails/docker compose/

--- iFreeLancer/settings.py
+++ iFreeLancer/settings.py
@@ -127,7 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.mail.ru'
+EMAIL_HOST = 'smtp.gmail.ru'
 EMAIL_USE_TLS = 'True'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
